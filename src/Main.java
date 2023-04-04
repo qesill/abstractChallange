7c12792f595f824adde49e7299c816d8039bd91a
Wiktor Andrzejkiewicz <wiktor.andrzejkiewicz@gmail.com>
2023-04-04 10:46:46 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        /*
+        1. Stwórz klasę abstrakcyjną Employee posiadającą:
+            a) pola protected String name i surname
+            b) konstruktor oraz gettery i settery dla powyższych pól
+            c) publiczną abstrakcyjną metodę toString zwracającą łańcuch znaków
+        2. Stwórz klasę Programmer rozszerzającą abstrakcyjną kllasę Employee:
+            a) dodaj do tej klasy pole String protected o nazwie languages
+            b) dodaj getter i setter
+            c) na koniec stwórz metodę toString, która prezentowała by dane z Employee i Programmer
+         */
+    }
+}
